a3d15d69bc6b7674fa4fd88b4e93c06a2ad97142
EvilNOP <evilnops@gmail.com>
2017-09-29 18:29:16 +0800

    Kept track of the connections and listened to the `data` event

--- tcp-chat/index.js
+++ tcp-chat/index.js
@@ -1,7 +1,24 @@
 const net = require('net');
+let count = 0;
 
 const server = net.createServer((conn) => {
-  console.log('new connection!');
+  conn.setEncoding('utf8');
+  
+  conn.write(
+      '\n > welcome to node-chat!'
+    + `\n > ${count} other people are connected at this time.`
+    + '\n > please write your name and press enter: '
+  );
+  
+  count++;
+  
+  conn.on('data', (chunk) => {
+    console.log(chunk);
+  });
+  
+  conn.on('close', () => {
+    count--;
+  });
 });
 
 server.listen(3000, () => {
