c320bee7337c43b88de32a3f63fe958c21657ad2
EvilNOP <evilnops@gmail.com>
2017-09-29 22:51:30 +0800

    Log the `connect` when connected

--- irc-client/index.js
+++ irc-client/index.js
@@ -5,6 +5,8 @@ const client = net.connect(6667, 'irc.freenode.net');
 client.setEncoding('utf8');
 
 client.on('connect', () => {
+  console.log('connect');
+  
   client.write('NICK mynick\r\n');
   client.write('USER mynick 0 * :realname\r\n');
   client.write('JOIN #node.js\r\n');
