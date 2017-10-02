4ba11ce1e8286805e841636f56feca017bbdc7ce
EvilNOP <evilnops@gmail.com>
2017-10-02 22:51:38 +0800

    Replaced `data` method to `query` method to form a query-string

--- express-tweet/search.js
+++ express-tweet/search.js
@@ -10,7 +10,7 @@ const request = require('superagent');
 
 module.exports = function search(query, fn) {
  request.get('http://search.twitter.com/search.json')
-  .data({ q: query })
+  .query({ q: query })
   .end((res) => {
     if (res.body && Array.isArray(res.body.results)) {
       return fn(null, res.body.results);
