a5f75459681458bb9c23ddd4ebb2e29072c5ae64
EvilNOP <evilnops@gmail.com>
2017-10-03 20:51:00 +0800

    Created `request-time` middleware

---
+++ simple-website/request-time.js
@@ -0,0 +1,30 @@
+/**
+ * Request time middleware.
+ *
+ * Options:
+ *   - 'time’ (‘Number’): number of ms after which you log (100)
+ *
+ * @param {Object} options
+ * @api public
+ */
+
+exports.exports = function (opts) {
+  let time = opts.tiime || 100;
+  
+  return function (req, res, next) {
+    let timer = setTimeout(() => {
+      console.log('is taking too long!', req.method, req.url);
+    }, time);
+    console.log('End: ');
+    console.log(res.end);
+    let end = res.end;
+    
+    res.end = function (chunk, encoding) {
+      res.end = end;
+      res.end(chunk, encoding);
+      clearTimeout(timer);
+    }
+    
+    next();
+  };
+};
