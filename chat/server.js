0a587cb13a605cca2b60c705453b175da54ddeab
EvilNOP <evilnops@gmail.com>
2017-10-05 22:42:44 +0800

    Fixed the usage of `socket.io` with `express`

--- chat/server.js
+++ chat/server.js
@@ -1,15 +1,16 @@
 const sio = require('socket.io');
+const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const server = http.Server(app);
+const io = sio(server);
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.listen(3000);
-
-const io = sio.listen(app);
 
 io.sockets.on('connection', (socket) => {
   console.log('Someone connected');
