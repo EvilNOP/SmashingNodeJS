92c4f1ad99454b21256430ff60e357df04a45223
EvilNOP <evilnops@gmail.com>
2017-10-06 23:09:11 +0800

    Created express server

---
+++ user-auth-example/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const cookieParser = require('cookie');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(session({ secret: 'my secret' }));
+
+app.set('view engine', 'jade');
+app.set('view options', { layout: false });
