cb993d2a05110ab1c2be4baec64a29aa6c18dd38
EvilNOP <evilnops@gmail.com>
2017-10-07 10:58:35 +0800

    Removed the white space

--- user-auth-example/server.js
+++ user-auth-example/server.js
@@ -13,7 +13,3 @@ app.use(session({ secret: 'my secret' }));
 
 app.set('view engine', 'jade');
 app.set('view options', { layout: false });
-<<<<<<< HEAD
-=======
-
->>>>>>> 6c091ba854913373f61f2c3c4669cc5bc25646a5
