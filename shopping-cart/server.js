e17536aac9548a6a1a57f4af99c4781da557ffe5
EvilNOP <evilnops@gmail.com>
2017-10-10 15:02:02 +0800

    Created express server

---
+++ shopping-cart/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+
+app.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+app.post('/create', (req, res, next) => {
+
+});
+
+app.get('/item/:id', (req, res, next) => {
+  res.render('item');
+});
+
+app.post('/item/:id/reivew', (req, res, next) => {
+
+});
+
+app.listen(3000, () => {
+  console.log(' - listening on http://*:3000');
+});
