82839113efa790ef90187989b44cc35135a0e429
EvilNOP <evilnops@gmail.com>
2017-10-10 21:12:56 +0800

    Used `body-parser` middleware

--- shopping-cart/server.js
+++ shopping-cart/server.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql');
 const express = require('express');
 const config = require('./config');
+const bodyParser = require('body-parser');
 
 const app = express();
 const db = mysql.createConnection(config);
@@ -8,8 +9,10 @@ const db = mysql.createConnection(config);
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index', { items: [] });
 });
 
 app.post('/create', (req, res, next) => {
