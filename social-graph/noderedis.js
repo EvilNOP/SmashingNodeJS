0910a3929b72fa81eabbeabab7fc61e2e75efcb1
EvilNOP <evilnops@gmail.com>
2017-10-12 11:02:26 +0800

    Defined graph methods

--- social-graph/noderedis.js
+++ social-graph/noderedis.js
@@ -23,6 +23,32 @@ User.prototype.save = function (fn) {
   client.hmset('user:' + this.id + ':data', this.data, fn);
 };
 
+User.prototype.follow = function (user_id, fn) {
+  client.multi()
+    .sadd('user:' + user_id + ':followers', this.id)
+    .sadd('user:' + this.id + ':follows', user_id )
+    .exec(fn);
+};
+
+User.prototype.unfollow = function (user_id, fn) {
+  client.multi()
+    .srem('user:' + user_id + ':followers', this.id)
+    .srem('user:' + this.id + ':follows', user_id )
+    .exec(fn);
+};
+
+User.prototype.getFollowers = function (fn) {
+  client.smembers('user:' + this.id + ':followers', fn);
+};
+
+User.prototype.getFollows = function (fn) {
+  client.smembers('user:' + this.id + ':follows', fn);
+};
+
+User.prototype.getFriends = function (fn) {
+  client.sinter('user:' + this.id + ':follows', 'users:' + this.id + 'followers', fn);
+};
+
 const client = redis.createClient();
 
 client.set('my key', 'my value', err => {
